0056b55dc7bc5ae016511cab8ecfd74ac52c7a58
Neeraj Babani <96947826+Neeraj-Babani@users.noreply.github.com>
2024-03-29 18:06:02 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,48 @@
+import openai
+import streamlit as st
+from sentence_transformers import SentenceTransformer
+from pinecone import Pinecone
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+os.environ['OPENAI_API_KEY']=os.getenv('OPENAI_API_KEY')
+ 
+model=SentenceTransformer("all-MiniLM-L6-v2")
+
+
+pc = Pinecone(api_key=os.getenv('PINECONE_API_KEY'))
+index = pc.Index('chatbot-langchain')
+
+
+def find_match(input):
+    input_em = model.encode(input).tolist()
+    result = index.query(vector=input_em, top_k=2, includeMetadata=True)
+    return result['matches'][0]['metadata']['text']+"\n"+result['matches'][1]['metadata']['text']
+
+def query_refiner(conversation, query):
+    client=openai.OpenAI()
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:"
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response.choices[0].message.content
+
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
+
+
